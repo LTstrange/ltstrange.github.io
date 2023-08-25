781b9b13695a6f791f6b7c23fa3d3d4ba40410a9
LTstrange <835422774@qq.com>
2023-08-25 22:27:06 +0800

    replace png logo with svg logo

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,20 @@
 module Main exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (class)
-import Html.Attributes exposing (src)
+import Html.Attributes exposing (src, alt)
 -- import Browser
 
 
 main : Html msg
 main = div [ class "container"] 
     [ header [] 
-    [ h1 [] [text "Welcome to my World!"]
-    ]
-    -- Show up
-    , section [] 
-        [ img [src "./assets/logo.png"] []
+        [ h1 [] [text "Welcome to my World!"]
+        
+        -- Show up
+        , img [src "./assets/logo.svg", alt "logo"] []
         , h3 [] [text "This is LTstrange!"]
         
-    -- Info
+        -- Info
         , h2 [] [text "Info"]
         , p [] [ text "Email: 835422774@qq.com"]
         ]
